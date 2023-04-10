3abf10cba4965e9c09a316136c8555c8dad49dcb
Alex <alex@a.com>
2023-04-10 12:53:48 +0300

    Added break with label to the lesson 7

--- lesson_7_loops.js
+++ lesson_7_loops.js
@@ -1,15 +1,15 @@
 'use strict'
 
-let i = 0;
+/* let i = 0;
 
 while (i < 5) {
     console.log(i);
     i++; // i = i + 1
-}
+} */
 
-console.log(i) // 5
+// console.log(i) // 5
 
-const validPassword = '123abc';
+// const validPassword = '123abc';
 
 
 /* while (true) {
@@ -28,12 +28,12 @@ const validPassword = '123abc';
 } */
 
 
-do {
+/* do {
     console.log(i);
     i--;
 } while (i < 5 && i > 0);
 
-console.log(i) // 0
+console.log(i) // 0 */
 
 
 /* for (let i = 0; i < 3; i++) {
@@ -51,7 +51,7 @@ console.log(i) // 0
     }
 } */
 
-for (; ;) {
+/* for (; ;) {
     const userPassword = prompt('Введите пароль', '');
 
     if (userPassword === null) {
@@ -64,4 +64,17 @@ for (; ;) {
     } else {
         alert('Пароль неверный!');
     }
+} */
+
+exit: 
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+        const data = prompt(`TR: ${i}; TD: ${j}`);
+        if (data === 'exit'){
+            alert('Exit');
+            break exit;
+        }
+    }
 }
+
+alert('Stop!');
--- test.js
+++ test.js
@@ -1,2 +1,25 @@
 'use strict'
 
+// check_target = False
+// for i in range(3):
+//     for j in range(3):
+//         target = int(input(f'TR: {i}; TD: {j}: '))
+//         if target == 10:
+//             check_target = True
+//             print("Цель поражена!")
+//             break 
+//     if check_target:
+//         break
+// else:
+//     print('Ты - лузер!')
+
+exit:
+for (let i = 1; i <= 3; i++){
+    for (let j = 1; j <= 3; j++){
+        let target = +prompt(`TR: ${i}; TD: ${j}`);
+        if (target === 0){
+            alert("Exit");
+            break exit;
+        }
+    }
+} 
