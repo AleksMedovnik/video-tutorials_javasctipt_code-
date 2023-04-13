b92cac866d45617ea178ca9f983d5fd62cddc2be
Alex <alex@a.com>
2023-04-13 12:21:07 +0300

    Added "parameters" to the lesson 12

--- lesson_12_functions.js
+++ lesson_12_functions.js
@@ -1,7 +1,28 @@
 'use strict'
 
-function checkPassword() {
-    const validPassword = "53679712";
+let user = 'John'; // global variable
+
+function sayHello() {
+    let user = 'Vasya'; // local variable
+    console.log(`Hello, ${user}!`);
+}
+
+
+sayHello(); // Hello, Vasya!
+sayHello(); // Hello, Vasya!
+console.log(user); // John
+
+
+function sayHi(user) {
+    console.log(`Hi, ${user}!`);
+}
+
+sayHi('Marie'); // Hi, Marie!
+sayHi('Tom'); // Hi, Tom!
+sayHi('John'); // Hi, John!
+
+
+function checkPassword(validPassword) {
     for (let i = 0; i < 3; i++) {
         const userPassword = prompt('Введите пароль', '');
 
@@ -19,16 +40,5 @@ function checkPassword() {
 }
 
 
-// checkPassword();
-
-let user = 'John'; // global variable
-
-function sayHello() {
-    let user = 'Vasya'; // local variable
-    console.log(`Hello, ${user}!`);
-}
-
-
-sayHello(); // Hello, Vasya!
-sayHello(); // Hello, Vasya!
-console.log(user); // John
+checkPassword('123abc');
+checkPassword('456zx');
