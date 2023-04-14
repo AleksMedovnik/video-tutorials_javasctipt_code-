fc402afc6c0e66679ec97078e024efe32d29d67d
Alex <alex@a.com>
2023-04-14 11:53:52 +0300

    Aded "return" to the lesson 12

--- lesson_12_functions.js
+++ lesson_12_functions.js
@@ -40,5 +40,44 @@ function checkPassword(validPassword) {
 }
 
 
-checkPassword('123abc');
-checkPassword('456zx');
+// checkPassword('123abc');
+// checkPassword('456zx');
+
+function sum(a, b){
+    return a + b;
+}
+
+console.log(sum(1, 2)); // 3
+
+let result = sum(4, 5);
+
+console.log(result); // 9
+
+function mul(a, b, c){
+    return a * b * c;
+}
+
+result = mul(2, 3, 5);
+console.log(result); // 30
+
+result = sayHello();
+console.log(result); // undefined
+// result = checkPassword('123abc');
+
+
+function f() {
+    console.log(1);
+    console.log(2);
+    // alert('Hello');
+    // prompt('Введите число: ');
+    return 1000, 10, 1;
+}
+
+let r = f();
+console.log(r); // 
+
+
+let d = double(10);
+console.log(d); // 20
+console.log(double(4)); // 8
+console.log(double(5)); // 10
